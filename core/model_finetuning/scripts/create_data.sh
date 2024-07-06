1d72ec1c5debb2e4a975fb670a937df2d3cca908
chen <cshao676@gmail.com>
2024-07-06 15:10:21 +0200

    update create data

--- core/model_finetuning/scripts/create_data.sh
+++ core/model_finetuning/scripts/create_data.sh
@@ -3,11 +3,8 @@
 #SBATCH --partition=cpuonly
 #SBATCH --job-name=tfidf
 
-
-
 #SBATCH --output=log/TAG_Benchmark_%j.output
 #SBATCH --error=error/TAG_Benchmark_%j.error
-
 
 #SBATCH --chdir=/hkfs/work/workspace/scratch/cc7738-benchmark_tag/TAPE_chen/batch
 
@@ -15,10 +12,10 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=cc7738@kit.edu
 
-source /hkfs/home/haicore/aifb/cc7738/anaconda3/etc/profile.d/conda.sh
+source /hkfs/home/project/hk-project-test-p0022257/cc7738/anaconda3/etc/profile.d/conda.sh
 
 conda activate base
-conda activate ss
+conda activate TAG-LP
 # <<< conda initialize <<<
 module purge
 module load devel/cmake/3.18
