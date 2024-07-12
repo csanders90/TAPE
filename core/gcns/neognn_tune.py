af9e8e67cf8827820d0abb82b0a5555ab201511e
ruiruiwang <wangruirui45@outlook.com>
2024-07-13 02:05:52 +0800

    fixed neognn problems arose from data

--- core/gcns/neognn_tune.py
+++ core/gcns/neognn_tune.py
@@ -75,9 +75,9 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--device', dest='device', required=True,
                         help='device id')
     parser.add_argument('--epochs', dest='epoch', type=int, required=False,
-                        default=100,
+                        default=50,
                         help='data name')
-    parser.add_argument('--repeat', type=int, default=2,
+    parser.add_argument('--repeat', type=int, default=1,
                         help='The number of repeated jobs.')
     parser.add_argument('--mark_done', action='store_true',
                         help='Mark yaml as done after a job has finished.')
@@ -149,11 +149,11 @@ if __name__ == "__main__":
             f"\n Valid: {2 * splits['train']['pos_edge_label'].shape[0]} samples,"
             f"\n Test: {2 * splits['test']['pos_edge_label'].shape[0]} samples")
         dump_cfg(cfg)
-        hyperparameter_search = {'hidden_channels': [64, 128, 256], 'num_layers': [2, 3],
-                                 'f_node_dim': [64, 128], 'f_global_dim': [64, 128], 'dropout': [0.0, 0.1, 0.3, 0.5],
+        hyperparameter_search = {'hidden_channels': [128, 256], 'num_layers': [2, 3],
+                                 'f_edge_dim': [8, 16, 32], 'f_node_dim': [64, 128], 'dropout': [0.0, 0.1, 0.3],
                              "batch_size": [512, 1024], "gnn_batch_size": [4096, 8192], "lr": [0.01, 0.001]}
         print_logger.info(f"hypersearch space: {hyperparameter_search}")
-        for hidden_channels, num_layers, f_node_dim, f_global_dim, dropout, batch_size, gnn_batch_size, lr in tqdm(
+        for hidden_channels, num_layers, f_node_dim, f_edge_dim, dropout, batch_size, gnn_batch_size, lr in tqdm(
                 itertools.product(*hyperparameter_search.values())):
             cfg.model.hidden_channels = hidden_channels
             cfg.train.batch_size = batch_size
@@ -161,12 +161,12 @@ if __name__ == "__main__":
             cfg.optimizer.lr = lr
             cfg.model.num_layers = num_layers
             cfg.model.f_node_dim = f_node_dim
-            cfg.model.f_global_dim = f_global_dim
+            cfg.model.f_edge_dim = f_edge_dim
             cfg.model.dropout = dropout
 
             print_logger.info(
                 f"hidden_channels: {hidden_channels}, num_layers: {num_layers}, f_node_dim: {f_node_dim}, "
-                f"f_global_dim: {f_global_dim}, dropout: {dropout}, batch_size: {batch_size}, gnn_batch_size: {gnn_batch_size}, lr: {lr}")
+                f"f_edge_dim: {f_edge_dim}, dropout: {dropout}, batch_size: {batch_size}, gnn_batch_size: {gnn_batch_size}, lr: {lr}")
             start_time = time.time()
             splits = ngnn_dataset(splits)
             model = NeoGNN(cfg.model.hidden_channels, cfg.model.hidden_channels,
@@ -220,7 +220,7 @@ if __name__ == "__main__":
 
             run_result.update(
                 {'hidden_channels': hidden_channels,' num_layers': num_layers, 'f_node_dim': f_node_dim,
-                'f_global_dim': f_global_dim, 'dropout': dropout, 'batch_size': batch_size, 'gnn_batch_size': gnn_batch_size, 'lr': lr})
+                'f_edge_dim': f_edge_dim, 'dropout': dropout, 'batch_size': batch_size, 'gnn_batch_size': gnn_batch_size, 'lr': lr})
             print_logger.info(run_result)
 
             to_file = f'{cfg.data.name}_{cfg.model.type}_tune_result.csv'
