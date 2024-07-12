3d061135a417e62c97d94ed0fac4c6bdd5465a9d
ruiruiwang <wangruirui45@outlook.com>
2024-07-13 01:57:19 +0800

    fixed neognn problems arose from data

--- core/embedding/embedding_LLM_tune.py
+++ core/embedding/embedding_LLM_tune.py
@@ -100,11 +100,11 @@ if __name__ == '__main__':
     cfg.device = args.device
     splits, text, data = load_data_lp[cfg.data.name](cfg.data)
     if cfg.embedder.type == 'minilm':
-        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-        node_features = model.encode(text)
+        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device=cfg.device)
+        node_features = model.encode(text, batch_size=256)
     elif cfg.embedder.type == 'e5-large':
-        model = SentenceTransformer('intfloat/e5-large-v2')
-        node_features = model.encode(text, normalize_embeddings=True)
+        model = SentenceTransformer('intfloat/e5-large-v2', device=cfg.device)
+        node_features = model.encode(text, normalize_embeddings=True, batch_size=256)
     elif cfg.embedder.type == 'llama':
         model_id = "meta-llama/Meta-Llama-3-8B"
         pipeline = transformers.pipeline(
@@ -113,9 +113,18 @@ if __name__ == '__main__':
         node_features = pipeline(text)
     elif cfg.embedder.type == 'bert':
         tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        model = BertModel.from_pretrained("bert-base-uncased")
-        encoded_input = tokenizer(text, return_tensors='pt', padding=True, truncation=True, max_length=512)
-        node_features = model(**encoded_input)
+        model = BertModel.from_pretrained("bert-base-uncased").to(cfg.device)
+        node_features = []
+        batch_size = 256  # Adjust batch size as needed to avoid OOM errors
+        for i in range(0, len(text), batch_size):
+            batch_texts = text[i:i + batch_size]
+            encoded_input = tokenizer(batch_texts, return_tensors='pt', padding=True, truncation=True,
+                                      max_length=512).to(cfg.device)
+            with torch.no_grad():
+                outputs = model(**encoded_input)
+                batch_features = outputs.pooler_output
+                node_features.append(batch_features)
+        node_features = torch.cat(node_features, dim=0)
     node_features = torch.tensor(node_features)
     print(node_features.shape)
 
