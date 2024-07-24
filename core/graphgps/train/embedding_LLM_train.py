74ebd8eabdec0fe97f24b994f54bd8aceb84ba0b
ruiruiwang <wangruirui45@outlook.com>
2024-07-25 07:17:20 +0800

    implemented cross decoder

--- core/graphgps/train/embedding_LLM_train.py
+++ core/graphgps/train/embedding_LLM_train.py
@@ -2,12 +2,9 @@ import os
 import sys
 import time
 from os.path import abspath, dirname, join
-
-import pandas as pd
 from torch.nn import BCEWithLogitsLoss
 
 sys.path.insert(0, abspath(join(dirname(dirname(__file__)))))
-from metrics_analysis.remap_and_visualize import visualize_weighted_adjacency_matrix
 
 import torch
 import wandb
@@ -150,55 +147,12 @@ class Trainer_embedding_LLM(Trainer):
         return result_mrr
 
     def finalize(self):
-        eval_data = self.test_data
-        start_train = time.time()
-
-        self.model.eval()
-        pos_edge = eval_data['pos_edge_label_index'].to(self.device)
-        neg_edge = eval_data['neg_edge_label_index'].to(self.device)
-        pos_edge_indices = []
-        neg_edge_indices = []
-        pos_pred = []
-        neg_pred = []
-
-        for perm in PermIterator(pos_edge.device, pos_edge.shape[1], self.batch_size, False):
-            pos_pred_batch = self.model(self.embedding[pos_edge][0], self.embedding[pos_edge][1]).squeeze().cpu()
-
-            pos_pred.append(pos_pred_batch)  # Append to list
-            pos_edge_indices.append(pos_edge[:, perm].cpu())
-
-        for perm in PermIterator(neg_edge.device, neg_edge.shape[1], self.batch_size, False):
-            neg_pred_batch = self.model(self.embedding[neg_edge][0], self.embedding[neg_edge][1]).squeeze().cpu()
-            neg_pred.append(neg_pred_batch)  # Append to list
-            neg_edge_indices.append(neg_edge[:, perm].cpu())
-
-        pos_pred = torch.cat(pos_pred, dim=0)
-        neg_pred = torch.cat(neg_pred, dim=0)
-
-        y_pred = torch.cat([pos_pred, neg_pred], dim=0)
-        pos_pred = pos_pred.detach().cpu()
-        neg_pred = neg_pred.detach().cpu()
-        result_mrr = get_metric_score(self.evaluator_hit, self.evaluator_mrr, pos_pred, neg_pred)
-        edge_index = torch.cat([pos_edge, neg_edge], dim=1)
-        pos_y = torch.ones(pos_edge.size(1))
-        neg_y = torch.zeros(neg_edge.size(1))
-        y_true = torch.cat([pos_y, neg_y], dim=0)
-        data_df = {
-            "edge_index0": edge_index[0].detach().cpu().numpy(),
-            "edge_index1": edge_index[1].detach().cpu().numpy(),
-            "pred": y_pred.detach().cpu().numpy(),
-            "gr": y_true.detach().cpu().numpy(),
-        }
-
-        df = pd.DataFrame(data_df)
-
-        auc = result_mrr['AUC']
-        mrr = result_mrr['MRR']
-        df.to_csv(f'{self.out_dir}/{self.name_tag}_AUC_{auc}_MRR_{mrr}.csv', index=False)
-        self.run_result['eval_time'] = time.time() - start_train
-        return
-
-
+        import time
+        for _ in range(1):
+            start_train = time.time()
+            self._evaluate(self.test_data)
+            self.run_result['eval_time'] = time.time() - start_train
+            
     def save_pred(self, pred, true, data):
         root = os.path.join(self.FILE_PATH, cfg.out_dir, 'pred_record')
         os.makedirs(root, exist_ok=True)
@@ -322,148 +276,6 @@ class Trainer_Triples(Trainer_embedding_LLM):
             start_train = time.time()
             self._evaluate(self.splits['test'])
             self.run_result['eval_time'] = time.time() - start_train
-
-
-class Trainer_embedding_LLM_Cross(Trainer):
-    def __init__(self,
-                 FILE_PATH: str,
-                 cfg: CN,
-                 model: torch.nn.Module,
-                 optimizer: torch.optim.Optimizer,
-                 splits: Dict[str, Data],
-                 run: int,
-                 repeat: int,
-                 loggers: Dict[str, Logger],
-                 print_logger: None,
-                 batch_size=None, ):
-        self.device = config_device(cfg).device
-        self.model = model.to(self.device)
-        self.model_name = cfg.model.type
-        self.data_name = cfg.data.name
-        self.FILE_PATH = FILE_PATH
-        self.epochs = cfg.train.epochs
-        self.run = run
-        self.repeat = repeat
-        self.loggers = loggers
-        self.print_logger = print_logger
-        self.batch_size = batch_size
-
-        self.test_data = splits['test'].to(self.device)
-        self.train_data = splits['train'].to(self.device)
-        self.valid_data = splits['valid'].to(self.device)
-        self.optimizer = optimizer
-        model_types = ['MLP-minilm', 'MLP-bert', 'MLP-llama', 'MLP-e5-large', 'MLP-tfidf', 'MLP-w2v']
-        self.test_func = {model_type: self._test for model_type in model_types}
-        self.evaluate_func = {model_type: self._evaluate for model_type in model_types}
-
-        self.evaluator_hit = Evaluator(name='ogbl-collab')
-        self.evaluator_mrr = Evaluator(name='ogbl-citation2')
-
-        self.run = run
-        self.repeat = repeat
-        self.results_rank = {}
-
-        self.name_tag = cfg.wandb.name_tag
-        self.run_result = {}
-
-        self.tensorboard_writer = writer
-        self.out_dir = cfg.out_dir
-        self.run_dir = None  # cfg.run_dir
-
-        self.report_step = 1
-
-    def _train_mlp(self):
-        self.model.train()
-        total_loss = 0
-        pos_train_edge = self.train_data['pos_edge_label_index'].T.to(self.device)
-        neg_train_edge = self.train_data['neg_edge_label_index'].T.to(self.device)
-
-        for perm in PermIterator(self.device, pos_train_edge.shape[0], self.batch_size):
-            self.optimizer.zero_grad()
-            pos_out = self.model(self.train_data.edge_features[perm])
-            pos_loss = -torch.log(pos_out + 1e-15).mean()
-
-            neg_out = self.model(self.train_data.edge_features[perm+pos_train_edge.shape[0]])
-            neg_loss = -torch.log(1 - neg_out + 1e-15).mean()
-
-            loss = pos_loss + neg_loss
-            loss.backward()
-            self.optimizer.step()
-            total_loss += loss.item() * self.batch_size
-
-        return total_loss / len(self.train_data)
-
-    def train(self):
-        for epoch in range(1, self.epochs + 1):
-            loss = self._train_mlp()
-            if epoch % int(self.report_step) == 0:
-                self.results_rank = self.merge_result_rank()
-
-                for key, result in self.results_rank.items():
-                    self.loggers[key].add_result(self.run, result)
-
-                    self.tensorboard_writer.add_scalar(f"Metrics/Train/{key}", result[0], epoch)
-                    self.tensorboard_writer.add_scalar(f"Metrics/Valid/{key}", result[1], epoch)
-                    self.tensorboard_writer.add_scalar(f"Metrics/Test/{key}", result[2], epoch)
-
-                    train_hits, valid_hits, test_hits = result
-                    if key in ['MRR', 'Hits@100', 'AUC']:
-                        self.print_logger.info(
-                            f'Run: {self.run + 1:02d}, Key: {key}, '
-                            f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {100 * train_hits:.2f}, Valid: {100 * valid_hits:.2f}, Test: {100 * test_hits:.2f}%')
-
-                self.print_logger.info('---')
-
-        return
-
-    @torch.no_grad()
-    def _evaluate(self, eval_data: Data):
-
-        self.model.eval()
-        pos_train_edge = eval_data['pos_edge_label_index'].T.to(self.device)
-        neg_train_edge = eval_data['neg_edge_label_index'].T.to(self.device)
-
-        pos_pred = self.model(eval_data.edge_features[:pos_train_edge.size(0)])
-        neg_pred = self.model(eval_data.edge_features[pos_train_edge.size(0):])
-        y_pred = torch.cat([pos_pred, neg_pred], dim=0)
-        y_pred = y_pred.view(-1).cpu()
-        hard_thres = (y_pred.max() + y_pred.min()) / 2
-        pos_y = torch.ones(pos_train_edge.size(0))
-        neg_y = torch.zeros(neg_train_edge.size(0))
-        y_true = torch.cat([pos_y, neg_y], dim=0)
-
-        pos_pred, neg_pred = y_pred[y_true == 1].cpu(), y_pred[y_true == 0].cpu()
-        y_pred = torch.where(y_pred >= hard_thres, torch.tensor(1), torch.tensor(0))
-        acc = torch.sum(y_true == y_pred) / len(y_true)
-
-        result_mrr = get_metric_score(self.evaluator_hit, self.evaluator_mrr, pos_pred, neg_pred)
-        result_mrr.update({'ACC': round(acc.tolist(), 5)})
-
-        return result_mrr
-
-    def finalize(self):
-        import time
-        for _ in range(1):
-            start_train = time.time()
-            self._evaluate(self.test_data)
-            self.run_result['eval_time'] = time.time() - start_train
-
-    def save_pred(self, pred, true, data):
-        root = os.path.join(self.FILE_PATH, cfg.out_dir, 'pred_record')
-        os.makedirs(root, exist_ok=True)
-        timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
-        file_path = os.path.join(root, f'{cfg.dataset.name}_{timestamp}.txt')
-
-        with open(file_path, 'w') as f:
-            for idx, subgraph in enumerate(data):
-                indices = torch.where(subgraph['z'] == 1)[0]
-                if len(indices) < 2:
-                    continue
-                corresponding_node_ids = subgraph['node_id'][indices]
-                pred_value = pred[idx]
-                true_value = true[idx]
-                f.write(
-                    f"{corresponding_node_ids[0].item()} {corresponding_node_ids[1].item()} {pred_value} {true_value}\n")
 
 
 class Trainer_embedding_LLM_Cross(Trainer):
